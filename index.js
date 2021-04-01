62e94690839eac25c36ab776a62933a8e116822a
pmcheraghi <pmahdicheraghi@gmail.com>
2021-04-01 20:40:07 +0430

    message corrected

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on("sticker", async(ctx, next) => {
+bot.on("sticker", async (ctx, next) => {
 	await ctx.reply(ctx.message.sticker.file_id);
 	// ctx.replyWithSticker(ctx.message.sticker.file_id);
 	next();
 });
 
-bot.on("animation", async(ctx, next) => {
+bot.on("animation", async (ctx, next) => {
 	await ctx.reply(ctx.message.animation.file_id);
 	// ctx.replyWithAnimation(ctx.message.animation.file_id);
 	next();
 });
 
-bot.on("message", async(ctx, next) => {
+bot.start((ctx, next) => {
+	if (!(ctx.message.forward_sender_name || ctx.message.forward_from))
+		ctx.reply("Forward some message to me\nor send me some sticker or GIF");
+	else
+		next();
+});
+
+bot.on("message", async (ctx) => {
 	if (ctx.message.forward_from) {
 		await ctx.reply("Forwarded from: " + ctx.message.forward_from.username);
 		await ctx.reply("ID: " + ctx.message.forward_from.id);
@@ -23,13 +30,9 @@ bot.on("message", async(ctx, next) => {
 		await ctx.reply("Forwarded from: " + ctx.message.forward_sender_name);
 		await ctx.reply("ID: " + "Access denied!");
 	}
-	else
-		next();
+	else if(!ctx.message.sticker && !ctx.message.animation)
+		ctx.reply("This is not a forwarded message!");
 });
-
-bot.start((ctx) => ctx.reply("Forward some message to me\nor send me some sticker or GIF"));
-
-bot.on("message", (ctx) => ctx.reply("This is not a forwarded message!"));
 
 bot.launch();
 
